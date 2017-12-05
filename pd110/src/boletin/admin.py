d2974c18f1331ea99ccc37b9f8e511c60f26e377
joyzac05 <yaya_joyza@hotmail.com>
2017-12-05 10:22:12 -0400

    creando aplicacion web con django

---
+++ pd110/src/boletin/admin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Registrado
+
+class AdminRegistrado(admin.ModelAdmin):
+	"""docstring for AdminRegistrado"""
+	list_display = ["email", "nombre", "timestamp"]
+	#list_display_links = ["nombre"]
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"]
+	search_fields = ["email", "nombre"]
+	class Meta:
+		model = Registrado
+		
+admin.site.register(Registrado, AdminRegistrado)
---
+++ pd110/src/boletin/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class RegForm(forms.Form):
+	nombre = forms.CharField(max_length=100)
+	email = forms.EmailField()
